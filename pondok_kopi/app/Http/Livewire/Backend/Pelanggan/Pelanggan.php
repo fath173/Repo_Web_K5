f719898ebcaa82198fc113b139099d1434d67c85
Fathor Rahman <fathorrahman2357@gmail.com>
2021-06-19 11:46:38 +0700

    -

--- pondok_kopi/app/Http/Livewire/Backend/Pelanggan/Pelanggan.php
+++ pondok_kopi/app/Http/Livewire/Backend/Pelanggan/Pelanggan.php
@@ -4,14 +4,22 @@ namespace App\Http\Livewire\Backend\Pelanggan;
 
 use Livewire\Component;
 use App\Http\Livewire\Backend\Redirec;
+use App\Models\User;
 
 class Pelanggan extends Component
 {
+
+
+
     public function render()
     {
         $redi = new Redirec();
         $redi->redirec();
 
-        return view('livewire.backend.pelanggan.pelanggan');
+        $dataPelanggan = User::where('level', 'pelanggan')->orderBy('id', 'DESC')->get();
+
+        return view('livewire.backend.pelanggan.pelanggan', [
+            'dataPelanggan' => $dataPelanggan
+        ]);
     }
 }
--- pondok_kopi/resources/views/livewire/backend/pelanggan/pelanggan.blade.php
+++ pondok_kopi/resources/views/livewire/backend/pelanggan/pelanggan.blade.php
@@ -1,1 +1,65 @@
-<h1>Halaman Pelanggan</h1>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <strong class="card-title">Data Table</strong>
+            </div>
+            <div class="card-body">
+                <table id="bootstrap-data-table" class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama Pelanggan</th>
+                            <th>Jenis Kelamin</th>
+                            <th>No Telepon</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($dataPelanggan as $key => $pelanggan)
+                            <tr>
+                                <td>
+                                    {{ $loop->iteration }}
+                                </td>
+                                <td>{{ date('d - m - Y', strtotime($pelanggan->name)) }}</td>
+                                <td>{{ $pelanggan->gender }}</td>
+                                <td>{{ $pelanggan->phone_number }} </td>
+                                <td><button type="button" class="btn btn-sm btn-info mb-1" data-toggle="modal"
+                                        data-target="#mediumModal{{ $pelanggan->id }}">
+                                        Detail
+                                    </button>
+                                </td>
+                            </tr>
+                            <div wire:ignore.self class="modal fade" id="mediumModal{{ $pelanggan->id }}"
+                                tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-lg" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                            <h5 class="modal-title" id="mediumModalLabel">Detail Pesanan:
+                                                <b>{{ $pelanggan->name }} {{ $pelanggan->id }}</b>
+                                            </h5>
+                                        </div>
+                                        <div class="modal-body">
+
+                                            <h1>Isi Detail data pelanggan disini!</h1>
+
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" wire:click="cancelOrder('{{ $pelanggan->id }}')"
+                                                class="btn btn-danger">Tolak</button>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
