da5db3e7ee61a98f798b100718a8a8d10f0faffe
anjualiciajohan <anjujohan0@gmail.com>
2021-06-20 18:51:29 +0700

    petugas

--- pondok_kopi/app/Http/Livewire/Backend/Petugas/Petugas.php
+++ pondok_kopi/app/Http/Livewire/Backend/Petugas/Petugas.php
@@ -4,14 +4,23 @@ namespace App\Http\Livewire\Backend\Petugas;
 
 use Livewire\Component;
 use App\Http\Livewire\Backend\Redirec;
+use App\Models\User;
 
 class Petugas extends Component
 {
+
+
+
     public function render()
     {
         $redi = new Redirec();
         $redi->redirec();
 
-        return view('livewire.backend.petugas.petugas');
+        //ini coding untuk memanggil data daari database
+        $dataPetugas = User::where('level', 'admin')->orderBy('id', 'DESC')->get();
+
+        return view('livewire.backend.petugas.petugas', [
+            'dataPetugas' => $dataPetugas
+        ]);
     }
 }
